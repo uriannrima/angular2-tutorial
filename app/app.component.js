bab72fe291cedf7daade8bb19c80ed5579eb30f1
Luciano Lima <luciano.lima@ingresso.com>
2016-05-23 17:31:01 -0300

    Master / Detail - Listagem de Herois (Array), definição de Evento, NgFor, NgIf, Styles do Componente.

--- app/app.component.js
+++ app/app.component.js
@@ -22,22 +22,38 @@ var AppComponent = (function () {
     function AppComponent() {
         // Titulo do App.
         this.title = 'Tour of Heroes';
-        // Heroi do App.
-        this.hero = {
-            id: 1,
-            name: 'Windstorm'
-        };
+        // Listagem de Herois.
+        this.heroes = HEROES;
     }
+    // Evento para selecionar heroi.
+    AppComponent.prototype.onSelect = function (hero) {
+        this.selectedHero = hero;
+    };
     AppComponent = __decorate([
         core_1.Component({
             // Seletor CSS para Bootstrap do Componente.
             selector: 'my-app',
             // Template do Componente.
-            template: "\n        <h1>{{title}}</h1>\n        <h2>{{hero.name}} details!</h2>\n        <div><label>id: </label>{{hero.id}}</div>\n        <div>\n            <label> name: </label>\n            <input [(ngModel)]=\"hero.name\" placeholder=\"Name\">\n        </div>\n    "
+            template: "\n        <!-- NgFor e NgIf s\u00E3o structural directives, pois mudam o corpo do DOM. -->\n        <!-- \"*\" determina que neste ponto ser\u00E1 criado um \"Template\", aonde seu escopo \u00E9 \"independente\". -->\n        <!-- Sendo assim \"let hero\" cria uma variavel \"hero\" somente para o escopo do NgFor. -->\n        <!-- Criando um evento (event)=\"methodName(parameter) para manipula\u00E7\u00E3o. \" -->\n        <!-- Bindings: (input) [output] [(two-way-binding)] -->\n        <!-- [class.cssClass] == true/false utilizado para definir um CSS num componente.. -->\n        \n        <h1>{{title}}</h1>\n        <h2>My Heroes</h2>\n        <ul class=\"heroes\">\n            <li *ngFor=\"let hero of heroes\"\n                [class.selected]=\"hero === selectedHero\"\n                (click)=\"onSelect(hero)\">\n                <span class=\"badge\">{{hero.id}}</span> {{hero.name}}\n            </li>\n        </ul>\n        <div *ngIf=\"selectedHero\">\n            <h2>{{selectedHero.name}} details!</h2>\n            <div><label>id: </label>{{selectedHero.id}}</div>\n            <div>\n                <label>name: </label>\n                <input [(ngModel)]=\"selectedHero.name\" placeholder=\"name\"/>\n            </div>\n        </div>\n    ",
+            // Styles do Componente.
+            styles: ["\n        .selected {\n            background-color: #CFD8DC !important;\n            color: white;\n        }\n        .heroes {\n            margin: 0 0 2em 0;\n            list-style-type: none;\n            padding: 0;\n            width: 15em;\n        }\n        .heroes li {\n            cursor: pointer;\n            position: relative;\n            left: 0;\n            background-color: #EEE;\n            margin: .5em;\n            padding: .3em 0;\n            height: 1.6em;\n            border-radius: 4px;\n        }\n        .heroes li.selected:hover {\n            background-color: #BBD8DC !important;\n            color: white;\n        }\n        .heroes li:hover {\n            color: #607D8B;\n            background-color: #DDD;\n            left: .1em;\n        }\n        .heroes .text {\n            position: relative;\n            top: -3px;\n        }\n        .heroes .badge {\n            display: inline-block;\n            font-size: small;\n            color: white;\n            padding: 0.8em 0.7em 0 0.7em;\n            background-color: #607D8B;\n            line-height: 1em;\n            position: relative;\n            left: -1px;\n            top: -4px;\n            height: 1.8em;\n            margin-right: .8em;\n            border-radius: 4px 0 0 4px;\n        }\n    "]
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
     return AppComponent;
 }());
 exports.AppComponent = AppComponent;
+// Stub de Herois.
+var HEROES = [
+    { "id": 11, "name": "Mr. Nice" },
+    { "id": 12, "name": "Narco" },
+    { "id": 13, "name": "Bombasto" },
+    { "id": 14, "name": "Celeritas" },
+    { "id": 15, "name": "Magneta" },
+    { "id": 16, "name": "RubberMan" },
+    { "id": 17, "name": "Dynama" },
+    { "id": 18, "name": "Dr IQ" },
+    { "id": 19, "name": "Magma" },
+    { "id": 20, "name": "Tornado" }
+];
 //# sourceMappingURL=app.component.js.map
--- app/app.component.ts
+++ app/app.component.ts
@@ -11,25 +11,112 @@ export class Hero {
 @Component({
     // Seletor CSS para Bootstrap do Componente.
     selector: 'my-app',
-    
+
     // Template do Componente.
     template: `
+        <!-- NgFor e NgIf são structural directives, pois mudam o corpo do DOM. -->
+        <!-- "*" determina que neste ponto será criado um "Template", aonde seu escopo é "independente". -->
+        <!-- Sendo assim "let hero" cria uma variavel "hero" somente para o escopo do NgFor. -->
+        <!-- Criando um evento (event)="methodName(parameter) para manipulação. " -->
+        <!-- Bindings: (input) [output] [(two-way-binding)] -->
+        <!-- [class.cssClass] == true/false utilizado para definir um CSS num componente.. -->
+        
         <h1>{{title}}</h1>
-        <h2>{{hero.name}} details!</h2>
-        <div><label>id: </label>{{hero.id}}</div>
-        <div>
-            <label> name: </label>
-            <input [(ngModel)]="hero.name" placeholder="Name">
+        <h2>My Heroes</h2>
+        <ul class="heroes">
+            <li *ngFor="let hero of heroes"
+                [class.selected]="hero === selectedHero"
+                (click)="onSelect(hero)">
+                <span class="badge">{{hero.id}}</span> {{hero.name}}
+            </li>
+        </ul>
+        <div *ngIf="selectedHero">
+            <h2>{{selectedHero.name}} details!</h2>
+            <div><label>id: </label>{{selectedHero.id}}</div>
+            <div>
+                <label>name: </label>
+                <input [(ngModel)]="selectedHero.name" placeholder="name"/>
+            </div>
         </div>
-    `
+    `,
+
+    // Styles do Componente.
+    styles:[`
+        .selected {
+            background-color: #CFD8DC !important;
+            color: white;
+        }
+        .heroes {
+            margin: 0 0 2em 0;
+            list-style-type: none;
+            padding: 0;
+            width: 15em;
+        }
+        .heroes li {
+            cursor: pointer;
+            position: relative;
+            left: 0;
+            background-color: #EEE;
+            margin: .5em;
+            padding: .3em 0;
+            height: 1.6em;
+            border-radius: 4px;
+        }
+        .heroes li.selected:hover {
+            background-color: #BBD8DC !important;
+            color: white;
+        }
+        .heroes li:hover {
+            color: #607D8B;
+            background-color: #DDD;
+            left: .1em;
+        }
+        .heroes .text {
+            position: relative;
+            top: -3px;
+        }
+        .heroes .badge {
+            display: inline-block;
+            font-size: small;
+            color: white;
+            padding: 0.8em 0.7em 0 0.7em;
+            background-color: #607D8B;
+            line-height: 1em;
+            position: relative;
+            left: -1px;
+            top: -4px;
+            height: 1.8em;
+            margin-right: .8em;
+            border-radius: 4px 0 0 4px;
+        }
+    `]
 })
 export class AppComponent {
     // Titulo do App.
     title = 'Tour of Heroes';
-    
-    // Heroi do App.
-    hero: Hero = {
-        id: 1,
-        name: 'Windstorm'
-    };
+
+    // Listagem de Herois.
+    heroes = HEROES;
+
+    // Heroi selecionado.
+    selectedHero: Hero;
+
+    // Evento para selecionar heroi.
+    onSelect(hero: Hero) {
+        this.selectedHero = hero;
+    }
 }
+
+// Stub de Herois.
+var HEROES: Hero[] = [
+    { "id": 11, "name": "Mr. Nice" },
+    { "id": 12, "name": "Narco" },
+    { "id": 13, "name": "Bombasto" },
+    { "id": 14, "name": "Celeritas" },
+    { "id": 15, "name": "Magneta" },
+    { "id": 16, "name": "RubberMan" },
+    { "id": 17, "name": "Dynama" },
+    { "id": 18, "name": "Dr IQ" },
+    { "id": 19, "name": "Magma" },
+    { "id": 20, "name": "Tornado" }
+];
